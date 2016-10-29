46b5c927c7feb2b1b94ec55ddb6c20bd4d6439da
Jaakko Pallari <jaakkop@cakesolutions.net>
2016-10-29 17:43:36 +0100

    Users referenced using IDs

--- app/model/BidChampData.scala
+++ app/model/BidChampData.scala
@@ -1,8 +1,10 @@
 package model
 
+import java.util.UUID
+
 case class BidChampData(
   items: Map[String, Item] = Map.empty,
-  users: Map[String, UserData] = Map.empty,
+  users: Map[UUID, UserData] = Map.empty,
   games: Map[Long, Game] = Map.empty,
   charity: Charity = Charity(0)
 ) {
@@ -17,16 +19,19 @@ case class BidChampData(
       justEvents("lollerz")
 
     case AddUser(username) =>
-      if (users.contains(username))
+      if (users.values.exists(_.user.name == username))
         justEvents(s"User '$username' already exists.")
       else {
-        (copy(users = users + (username -> UserData(User(username)))),
-          List(s"User '$username' created"))
+        val userData = UserData(User(username))
+        Result(
+          copy(users = users + (userData.id -> userData)),
+          List(s"User '$username' created")
+        )
       }
 
     case authCmd: AuthCommand =>
-      users.get(authCmd.user) match {
-        case None => justEvents(s"No such user '${authCmd.user}' found.")
+      users.get(authCmd.userId) match {
+        case None => justEvents(s"No such user '${authCmd.userId}' found.")
         case Some(userData) =>
           evalAuthCommand(userData.user, authCmd)
       }
@@ -39,8 +44,10 @@ case class BidChampData(
           justEvents(s"Item '${bid.item}' doesn't exist.")
         case Some(item) =>
           val game = Game.newInstance(nextGameId, item).upsertBid(user, bid.amount)
-          (updateGame(game),
-            List(s"Added game #${game.id} for item '${item.name}'."))
+          Result(
+            updateGame(game),
+            List(s"Added game #${game.id} for item '${item.name}'.")
+          )
       }
 
     case bid: AddToBid =>
@@ -48,8 +55,10 @@ case class BidChampData(
         case None => justEvents(s"No game #${bid.game} found.")
         case Some(game) if game.isActive =>
           val updatedGame = game.upsertBid(user, bid.amount)
-          (updateGame(updatedGame),
-            List(s"User '${user.name}' bid ${bid.amount} for item ${game.item.toString}."))
+          Result(
+            updateGame(updatedGame),
+            List(s"User '${user.name}' bid ${bid.amount} for item ${game.item.toString}.")
+          )
         case Some(game) =>
           justEvents(s"Can't bid on finished games.")
       }
@@ -57,28 +66,30 @@ case class BidChampData(
 
   private def updateGame(game: Game) = copy(games = games + (game.id -> game))
 
-  private def justEvents(events: Event*): Result = (this, events)
+  private def justEvents(events: Event*): Result = Result(this, events)
 }
 
 object BidChampData {
   type Event = String
-  type Result = (BidChampData, Seq[Event])
+  case class Result(state: BidChampData, events: Seq[Event])
 
   sealed trait Command
   sealed trait AuthCommand extends Command {
-    def user: String
+    def userId: UUID
   }
 
   object Refresh extends Command
-  case class AddUser(username: String) extends Command
-  case class StartBid(user: String, item: String, amount: Int) extends AuthCommand
-  case class AddToBid(user: String, game: Long, amount: Int) extends AuthCommand
+  case class AddUser(user: String) extends Command
+  case class StartBid(userId: UUID, item: String, amount: Int) extends AuthCommand
+  case class AddToBid(userId: UUID, game: Long, amount: Int) extends AuthCommand
 }
 
 case class UserData(
   user: User,
   spent: Int = 0
-)
+) {
+  def id = user.uuid
+}
 
 case class Charity(
   contributed: Int
